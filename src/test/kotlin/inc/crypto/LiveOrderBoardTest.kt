d6ca00bd7b9ae2ec4b4d3a275c29afd581edd6c2
diegogliosca <diego.gliosca@gmail.com>
2020-09-23 14:42:35 +0100

    created live order board

---
+++ src/test/kotlin/inc/crypto/LiveOrderBoardTest.kt
@@ -0,0 +1,25 @@
+package inc.crypto
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Test
+
+class LiveOrderBoardTest {
+
+    @Test
+    fun `can register an order`() {
+        val board = LiveOrderBoard()
+        val order = Order.Sell(1, "Bitcoin", 1, 10.20.toBigDecimal())
+
+        assertThat(board.register(order), equalTo(true))
+    }
+}
+
+class LiveOrderBoard {
+    private val ordersBook = OrdersBook()
+
+    fun register(order: Order): Boolean {
+        return ordersBook.registerOrder(order)
+    }
+
+}
