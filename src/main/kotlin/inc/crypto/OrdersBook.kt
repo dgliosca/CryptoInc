a6ddf80fd771aaa9d4eeea6c5eb541e00c013de1
diegogliosca <diego.gliosca@gmail.com>
2020-09-24 16:16:16 +0100

    clean up

--- src/main/kotlin/inc/crypto/OrdersBook.kt
+++ src/main/kotlin/inc/crypto/OrdersBook.kt
@@ -3,17 +3,11 @@ package inc.crypto
 class OrdersBook {
     private val orders = mutableListOf<Order>()
 
-    fun registerOrder(order: Order): Boolean {
-        return orders.add(order)
-    }
+    fun registerOrder(order: Order) = orders.add(order)
 
-    fun get(order: Order): Order? {
-        return orders.find { it == order }
-    }
+    fun get(order: Order) = orders.find { it == order }
 
-    fun cancelOrder(order: Order): Boolean {
-        return orders.remove(order)
-    }
+    fun cancelOrder(order: Order) = orders.remove(order)
 
     fun orders(): List<Order> = orders
 }
